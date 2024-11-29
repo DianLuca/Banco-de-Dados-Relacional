79eed6b739f62ef8e342543c9bcf9667d14baaa7
DianLuca <dianlucca@hotmail.com>
2024-11-28 21:35:37 -0300

    Criação dos arquivo Aula001_update.py

---
+++ Aula001_update.py
@@ -0,0 +1,18 @@
+import os
+import sqlite3
+
+
+conn = sqlite3.connect('C:\Repositorios\Banco-de-Dados-Relacional\BD_SQLite\meu_banco.db')
+
+cursor = conn.cursor()
+
+os.system('cls')
+nome_cliente = input('Digite o nome do cliente: ')
+nova_idade = int(input('Digite a nova idade: '))
+
+# Atualiza a idade com base no nome fornecido pelo usuário
+cursor.execute('UPDATE clientes SET idade = ? WHERE nome = ?', (nova_idade, nome_cliente))
+
+conn.commit()
+print('Dados atualizados com sucesso!')
+conn.close()
