6b38e415b71082f6cf28524bee04569fb4e30c75
DianLuca <dianlucca@hotmail.com>
2024-11-28 21:27:55 -0300

    Criando o arquivo de exclusao.py

---
+++ BD_SQLite/Aula001_exclusao.py
@@ -0,0 +1,19 @@
+import os
+import sqlite3
+
+
+conn = sqlite3.connect('C:\Repositorios\Banco-de-Dados-Relacional\BD_SQLite\meu_banco.db')
+
+cursor = conn.cursor()
+
+os.system('cls')
+
+nome_cliente = input('Digite o nome do usuário que você deseja excluir? ')
+
+# Executa a exclusão com base no nome fornecido
+cursor.execute('DELETE FROM clientes WHERE nome = ?', (nome_cliente,))
+conn.commit()
+
+print('Cliente deletado com sucesso!')
+
+conn.close()
